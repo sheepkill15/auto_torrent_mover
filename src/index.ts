20224442cad33893c60a8903f2248b888e9d5838
Peter Simon <simon.peter199999@gmail.com>
2021-04-11 17:06:58 +0300

    Added config file loader & logger + category and fileCount checks

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,81 @@
-#!/usr/bin/env node
 import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
 import path from "path";
 import fs from "fs";
+import { Arguments, InferredOptionTypes } from "yargs";
+import { Logger, LogLevel } from "./Logger";
+import { ConfigLoader } from "./ConfigLoader";
 
 const fileRegex = /\] (.+?)((\[)|( \- [0-9]+))/g;
 
-const assertFile = (file: string, folder: string) => {
+const config = new ConfigLoader();
+const logger = new Logger(
+  config.get("logFileName") || "myScript.log",
+  LogLevel[config.get("logLevel") as keyof typeof LogLevel] || LogLevel.BOTH
+);
+config.printConfig(logger);
+
+yargs(hideBin(process.argv))
+  .options({
+    file: { type: "string", demandOption: true },
+    savePath: { type: "string", demandOption: true },
+    fileCount: { type: "number", demandOption: false },
+    category: { type: "string", demandOption: false },
+  })
+  .command(
+    "assert [file] [savePath] [fileCount] [category]",
+    "create and put file in matching directory",
+    (yargs) => {
+      yargs.positional("file", {
+        describe: "file to assert",
+        type: "string",
+        default: "",
+        alias: "f",
+      });
+      yargs.positional("savePath", {
+        describe: "path to file",
+        type: "string",
+        default: "",
+        alias: "s",
+      });
+      yargs.positional("fileCount", {
+        describe: "number of files to process (currently only 1 is supported)",
+        type: "number",
+        default: "",
+        alias: "n",
+      });
+      yargs.positional("category", {
+        describe: "category of the file",
+        type: "string",
+        default: "",
+        alias: "c",
+      });
+    },
+    (argv) => {
+      if (!validateInput(argv)) {
+        return;
+      }
+
+      assertFile(argv["file"], argv["savePath"]);
+    }
+  )
+  .showHelpOnFail(true)
+  .parse();
+
+function assertFile(file: string, folder: string) {
   if (!file || !folder) {
-    console.log("NO");
+    logger.log("NO");
     return;
   }
 
   try {
     var name = fileRegex.exec(file)?.[1];
     if (!name) {
-      console.log("Regex fail");
+      logger.log("Regex fail");
       return;
     }
   } catch (e) {
-    console.log(e);
+    logger.log(e);
     return;
   }
 
@@ -32,36 +88,47 @@ const assertFile = (file: string, folder: string) => {
     const src = path.join(folder, file);
     fs.copyFileSync(src, path.join(dest, file));
     fs.rmSync(src);
-    console.log("Moved!");
-    console.log(`Source: ${src}\nDest: ${dest}`);
+    logger.log("Moved!");
+    logger.log(`Source: ${src}\nDest: ${dest}`);
   } catch (e) {
-    console.log(e);
+    logger.log(e);
   }
-};
+}
 
-yargs(hideBin(process.argv))
-  .options({
-    file: { type: "string", demandOption: true },
-    savePath: { type: "string", demandOption: true },
-  })
-  .command(
-    "assert [file] [savePath]",
-    "create and put file in matching directory",
-    (yargs) => {
-      yargs.positional("file", {
-        describe: "file to assert",
-        type: "string",
-        default: "",
-      });
-      yargs.positional("savePath", {
-        describe: "path to file",
-        type: "string",
-        default: "",
-      });
-    },
-    (argv) => {
-      assertFile(argv["file"], argv["savePath"]);
-    }
-  )
-  .showHelpOnFail(true)
-  .parse();
+type ArgvType = Arguments<
+  Omit<{}, "file" | "savePath" | "fileCount" | "category"> &
+    InferredOptionTypes<{
+      file: {
+        type: "string";
+        demandOption: true;
+      };
+      savePath: {
+        type: "string";
+        demandOption: true;
+      };
+      fileCount: {
+        type: "number";
+        demandOption: false;
+      };
+      category: {
+        type: "string";
+        demandOption: false;
+      };
+    }>
+>;
+
+function validateInput(argv: ArgvType): boolean {
+  const fileCount = argv["fileCount"];
+  if (fileCount && fileCount != 1) {
+    logger.log(`Invalid input: fileCount: ${fileCount}`);
+    return false;
+  }
+
+  const category = argv["category"];
+  if (category && category !== (config.get("defaultCategory") || "Anime")) {
+    logger.log(`Invalid input: category: ${category}`);
+    return false;
+  }
+
+  return true;
+}
